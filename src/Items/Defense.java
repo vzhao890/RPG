1c0aa0f36a8c2c8c812b2e9a668c0c5bf12689f8
BT_1N3_19 <tlee3526@bths.edu>
2019-02-01 10:08:23 -0500

    hi

--- src/Items/Defense.java
+++ src/Items/Defense.java
@@ -2,10 +2,10 @@ package Items;
 
 public class Defense extends items {
     String[] giveDefense = {
-            "Raggity Woodplank",//Starting Defense          Set Defense:75
-            "Adequate Armor",//Drop Rate:80%                Set Defense:85
-            "Knights Vow",//Drop Rate:50%                   Set Defense 100
-            "Ultimate Mystical Shiny Armor"};//Drop Rate:1% Set Defense: 125
+            "Raggity Woodplank",//Starting Defense     Defense:75
+            "Adequate Armor",//Drop Rate:80%           Defense:85
+            "Knights Vow",//Drop Rate:50%              Defense 100
+            "Ultimate Mystical Shiny Armor"};//Drop Rate:1%
 
     public int giveAttack() {
         return 0;
