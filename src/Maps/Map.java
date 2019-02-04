a7c5f46edd2f8084356294a527ce94e9574e4eb8
BT_1N3_20 <@>
2019-02-04 10:13:14 -0500

    @@@

--- src/Maps/Map.java
+++ src/Maps/Map.java
@@ -1,4 +1,19 @@
 package Maps;
+import Rooms.*;
+public class Map
+{
+    int[][] basicmap = new int[20][20];
+    public int[] printMap()
+    {
+        for(int i =0; i< basicmap.length;i++)
+        {
+            if((i==0)||(i==19))
+            {
+                for(int j = 0; j<basicmap[i].length;j++)
+                {
 
-public class Map {
+                }
+            }
+        }
+    }
 }
